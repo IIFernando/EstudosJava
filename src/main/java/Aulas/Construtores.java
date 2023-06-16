db0a6bb80331fc36a5a380654587df6727b844d4
Fernando <fernandoaraujocontato@outlook.com>
2023-06-15 22:02:39 -0300

    Atualização aulas

--- src/main/java/Aulas/Construtores.java
+++ src/main/java/Aulas/Construtores.java
@@ -17,15 +17,20 @@ public class Construtores {
         System.out.print("Price: ");
         double price = sc.nextDouble();
 
-        System.out.println("Quantity in stock: ");
-        int quantity = sc.nextInt();
+//        System.out.println("Quantity in stock: ");
+//        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
+
+        product.setName("Computer");
+        System.out.println("Updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Updated price: " + product.getPrice());
 
         System.out.println("Product data: " + product);
 
         System.out.println("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
         System.out.println("Update data: " + product);
 
